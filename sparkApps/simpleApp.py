b3355276820f99b777a03a6edfdbc97381f7c8fd
tbonza <tylers.pile@gmail.com>
2015-10-02 18:51:14 -0400

    configuring spark apps

---
+++ sparkApps/simpleApp.py
@@ -0,0 +1,14 @@
+""" SimpleApp.py
+
+Demo for making a spark app
+"""
+from pyspark import SparkContext
+
+logFile = "YOUR_SPARK_HOME/README.md"  # Should be some file on your system
+sc = SparkContext("local", "Simple App")
+logData = sc.textFile(logFile).cache()
+
+numAs = logData.filter(lambda s: 'a' in s).count()
+numBs = logData.filter(lambda s: 'b' in s).count()
+
+print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
