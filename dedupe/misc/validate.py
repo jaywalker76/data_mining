beed5b51e40f74218d2333de2f374ae33971a7b4
tbonza <tylers.pile@gmail.com>
2015-10-09 16:32:40 -0400

    fixed dbpedia csv but not monopedia

--- dedupe/misc/validate.py
+++ dedupe/misc/validate.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-""" Validate data for csv files 
+""" Validate and fix data for csv files 
 
-There was an issue with unbalanced quotes in the example data. Resolving it.
+1. There is an issue with unbalanced quotes in the example data. 
+   Resolving it.
+
+2. There is an issue with empty lines in the example data.
+   Unresolved ***
+
 """
-import sys
-import re
 import argparse
+import csv
 
 # script arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("csv", help="path to csv file")
 parser.add_argument("cols", help="Number of columns in csv file")
 args = parser.parse_args()
+
+########################################################################
+## Utils
+########################################################################
 
 def read_csv():
     with open(args.csv, 'r') as infile:
@@ -20,10 +28,30 @@ def read_csv():
 
     return read_data
 
+def write_csv(write_data):
+    outfile = args.csv.split(".")
+    _outfile = outfile[0] + "_valid."
+    outfile = _outfile + outfile[1]
+
+    try:
+        with open(outfile, 'w'):
+            csvwriter = csv.writer(outfile, delimiter=",",
+                                   quotechar='"', quoting=csv.QUOTE_ALL)
+
+            for line in write_data.split("\n"):
+                csvwriter.writerow(line)
+
+            outfile.close()
+
+        return True
+
+    except:
+        return False
 
 
-
-# tests for data
+########################################################################
+## Tests for data
+########################################################################
 
 def validateQuotes(read_data):
     """ Resolving CSV input error
@@ -42,7 +70,7 @@ def validateQuotes(read_data):
 
     if quote_count % int(args.cols) != 0:
         print("Unbalanced quote in CSV file")
-        raise(ValueError)
+        return False
 
     else:
         return True
@@ -62,19 +90,80 @@ def validateNewline(read_data):
 
     if newline_count % int(args.cols) != 0:
         print("Missing newline characters")
-        raise(ValueError)
+        return False
     else:
         return True
+
+##################################################################
+## Fixes for data
+##################################################################
+
+def itemLenSanity(item):
+    """ Check for 0 len """
+    if len(item) > 0:
+        return True
+    else:
+        return False
+    
+def fixNewline(read_data):
+    """ Missing newline characters were found in the csv file; fixing """
+    items = read_data.split(",")
+    ncols = int(args.cols)
+
+    fixed_items = []
+    
+    for item in items:
+
+        count = 0
+
+        while count < ncols:
+
+            if not itemLenSanity(item): # problem area
+                break
+            
+            if count == (ncols - 1) and item[-1] != "\n":
+                item += "\n"
+
+            count += 1
+
+        fixed_items.append(item)
+
+    # list to string
+    new_csv = ""
+
+    for fixed in fixed_items:
+
+        col = 0
+        while col < (ncols - 1):
+            new_csv += fixed + ","
+            col += 1
+
+    return new_csv
+
+def testerFixer(data):
+    print("go go testerFixer")
+    
+    if not validateQuotes(data):
+        print('validateQuotes failed')
+        
+    if not validateNewline(data):
+        print ('validateNewline failed')
+        data = fixNewline(data)
+        testerFixer(data)
+
+    if validateNewline(data) and validateQuotes(data):
+        return data
 
 
 def main():
     # setUp
     read_data = read_csv()
 
-    # tests
-    assert validateQuotes(read_data)
-    assert validateNewline(read_data)
+    # test & fix
+    write_data = testerFixer(read_data)
 
+    # write to file
+    write_csv(write_data)
 
     print("Test complete")
 
