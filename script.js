520665069f2c814c573eb8c880a397ca12ac33ac
tbonza <tylers.pile@gmail.com>
2015-10-29 09:41:53 -0400

    updated project page

---
+++ script.js
@@ -0,0 +1,15 @@
+C.initialize({
+  client_id: '4660663e7f60a0886584e07a4bc7a9dc',
+    redirect_uri: 'http://tbonza.github.io/soundcloud.html'
+    });
+
+    $(document).ready(function() {
+      $('a.connect').click(function(e) {
+       e.preventDefault();
+       SC.connect(function(){
+       SC.get('/me', function(me) {
+        $('#username').html(me.username);
+      });
+    });
+  });
+});
