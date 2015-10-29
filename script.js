882f6beb9b67587346821c648fa20661c8ea6887
tbonza <tylers.pile@gmail.com>
2015-10-29 09:45:23 -0400

    updated to correct soundcloud.html path

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 C.initialize({
   client_id: '4660663e7f60a0886584e07a4bc7a9dc',
-    redirect_uri: 'http://tbonza.github.io/soundcloud.html'
+    redirect_uri: 'http://tbonza.github.io/data_mining/soundcloud.html'
     });
 
     $(document).ready(function() {
