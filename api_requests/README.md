60cf49ddc21aacd91890d7afa9d8317f42ca72e8
tbonza <tylers.pile@gmail.com>
2015-10-29 09:29:03 -0400

    adding api stuff

---
+++ api_requests/README.md
@@ -0,0 +1,5 @@
+Api requests for easier data mining
+====================================
+
+Life would be better if API requests were used instead
+of the GUI. This allows for a scripting capability.
---
+++ api_requests/script.js
@@ -0,0 +1,15 @@
+SC.initialize({
+  client_id: '4660663e7f60a0886584e07a4bc7a9dc',
+  redirect_uri: 'http://external.codecademy.com/soundcloud.html'
+});
+
+$(document).ready(function() {
+  $('a.connect').click(function(e) {
+    e.preventDefault();
+    SC.connect(function(){
+        SC.get('/me', function(me) {
+            $('#username').html(me.username);
+        });
+    });
+  });
+});
