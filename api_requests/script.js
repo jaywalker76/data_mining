c0f04344c934f160b3738a758a7755a05da5d8ce
tbonza <tylers.pile@gmail.com>
2015-10-29 10:54:35 -0400

    updating api_requests with soundcloud example

--- api_requests/script.js
+++ api_requests/script.js
@@ -1,15 +1,15 @@
 SC.initialize({
   client_id: '4660663e7f60a0886584e07a4bc7a9dc',
-  redirect_uri: 'http://external.codecademy.com/soundcloud.html'
-});
+    redirect_uri: 'http://tbonza.github.io/data_mining/soundcloud.html'
+    });
 
-$(document).ready(function() {
-  $('a.connect').click(function(e) {
-    e.preventDefault();
-    SC.connect(function(){
-        SC.get('/me', function(me) {
-            $('#username').html(me.username);
-        });
+    $(document).ready(function() {
+      $('a.connect').click(function(e) {
+       e.preventDefault();
+       SC.connect(function(){
+       SC.get('/me', function(me) {
+        $('#username').html(me.username);
+      });
     });
   });
 });
